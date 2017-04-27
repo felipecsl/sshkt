60aa0cac02f966f189a270e523217e8f0fc418f4
Felipe Lima <felipe.lima@gmail.com>
2017-04-27 01:13:09 -0700

    Add integration test

--- sshkt/src/main/kotlin/sshkt/Host.kt
+++ sshkt/src/main/kotlin/sshkt/Host.kt
@@ -1,8 +1,15 @@
 package sshkt
 
+import net.schmizz.sshj.common.LoggerFactory
 import net.schmizz.sshj.connection.channel.direct.Session
+import java.util.concurrent.TimeUnit
 
-class Host(val hostname: String, private val session: Session) {
+class Host(
+    val hostname: String,
+    loggerFactory: LoggerFactory,
+    private val session: Session) {
+  private val logger = loggerFactory.getLogger(javaClass)
+
   fun capture(vararg args: String) {
 
   }
@@ -11,7 +18,14 @@ class Host(val hostname: String, private val session: Session) {
 
   }
 
-  fun execute(vararg args: String) {
-    session.exec(args.joinToString())
+  fun execute(vararg args: String): Int {
+    return session.exec(args.joinToString()).let { command ->
+      command.inputStream.bufferedReader().use {
+        val output = it.readText()
+        logger.info("[exec]: $output")
+        command.join(5, TimeUnit.SECONDS)
+        command.exitStatus
+      }
+    }
   }
 }
--- sshkt/src/main/kotlin/sshkt/SshKt.kt
+++ sshkt/src/main/kotlin/sshkt/SshKt.kt
@@ -1,19 +1,37 @@
 package sshkt
 
 import net.schmizz.sshj.SSHClient
+import net.schmizz.sshj.common.LoggerFactory
 
-class SshKt(private val ssh: SSHClient = SSHClient()) {
+class SshKt(
+    private val sshClient: SSHClient = SSHClient(),
+    private val loggerFactory: LoggerFactory = LoggerFactory.DEFAULT) {
   fun on(servers: List<String>, block: (Host) -> Unit) {
     servers.forEach { host ->
-      ssh.connect(host)
-      try {
-        ssh.authPublickey("root")
-        ssh.startSession().use { session ->
-          block.invoke(Host(host, session))
+      val parser = findSuitableParser(host)
+      sshClient.connect(parser.hostname(), parser.port())
+      // TODO: Figure out authentication
+//      sshClient.authPublickey("root")
+      sshClient.authPassword(parser.username(), parser.username())
+      sshClient.use {
+        it.startSession().use { session ->
+          block.invoke(Host(host, loggerFactory, session))
         }
-      } finally {
-        ssh.disconnect()
       }
     }
+  }
+
+  private fun findSuitableParser(hostString: String): HostParser {
+    return HOST_PARSERS
+        .map { it.constructors[0] }
+        .map { it.newInstance(hostString) as HostParser }
+        .firstOrNull { it.suitable() }
+        ?: throw UnparseableHostStringException("Cannot parse host string $hostString")
+  }
+
+  companion object {
+    val HOST_PARSERS: List<Class<out HostParser>> = listOf(
+        SimpleHostParser::class.java,
+        HostWithUsernameAndPortParser::class.java)
   }
 }
